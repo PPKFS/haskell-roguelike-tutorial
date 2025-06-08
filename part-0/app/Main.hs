a72179b5aea39c1b321a3407141dc3a69f77afc8
avery <thecommunistduck@hotmail.co.uk>
2025-06-08 14:22:01 +0200

    git subrepo clone --branch=part-0 https://github.com/ppkfs/haskell-roguelike-tutorial part-0
    subrepo:
      subdir:   "part-0"
      merged:   "82524fc"
    upstream:
      origin:   "https://github.com/ppkfs/haskell-roguelike-tutorial"
      branch:   "part-0"
      commit:   "82524fc"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "4f60dd7"

---
+++ part-0/app/Main.hs
@@ -0,0 +1,34 @@
+module Main where
+
+import Prelude -- we'll make our own prelude in part 1
+
+import BearLibTerminal ( terminalRefresh )
+-- note that pattern synonyms don't like manual imports
+import BearLibTerminal.Keycodes
+import Control.Monad (when)
+import Rogue.Config ( WindowOptions(..), defaultWindowOptions )
+import Rogue.Events ( BlockingMode(..), handleEvents )
+import Rogue.Geometry.V2 ( V2(..) )
+import Rogue.Window ( withWindow )
+
+screenSize :: V2
+screenSize = V2 100 50
+
+main :: IO ()
+main =
+  withWindow
+  defaultWindowOptions { size = Just screenSize }
+  (return ()) -- no init logic
+  (const runLoop)
+  (return ()) -- no shutdown logic
+
+runLoop :: IO ()
+runLoop = do
+  terminalRefresh
+  -- event handling
+  shouldContinue <- handleEvents Blocking $ \case
+    TkResized -> return True
+    TkClose -> return False
+    TkEscape -> return False
+    _ -> return True
+  when (and shouldContinue) runLoop
